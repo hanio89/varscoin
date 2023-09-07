5b969506217864f3833c2b81cf6d5ec03118bb4b
root <hannguyen2034@gmail.com>
2023-09-07 11:11:48 +0100

    update

--- src/main.js
+++ src/main.js
@@ -35,7 +35,7 @@ VarsCoin.minePendingTransactions(myWalletAddress);
 
 console.log();
 console.log(
-  `Balance of xavier is ${VarsCoin.getBalanceOfAddress(myWalletAddress)}`
+  `Balance of varsier is ${VarsCoin.getBalanceOfAddress(myWalletAddress)}`
 );
 
 // Uncomment this line if you want to test tampering with the chain
@@ -44,3 +44,56 @@ console.log(
 // Check if the chain is valid
 console.log();
 console.log('Blockchain valid?', VarsCoin.isChainValid() ? 'Yes' : 'No');
+
+
+// let varsCoin = new Blockchain();
+// //part4
+// const tx1 = new Transaction(myWalletAddress, 'public key goes here', 10);
+// tx1.signTransaction(myKey);
+// varsCoin.addTransaction(tx1);
+
+// console.log('\n Starting the miner...');
+// varsCoin.minePendingTransactions(myWalletAddress);
+
+// console.log('\nBalance of varsiers is', varsCoin.getBalanceOfAddress(myWalletAddress));
+// varsCoin.chain[1].transactions[0].amount = 1;
+
+// console.log('Is chain valid?', varsCoin.isChainValid());
+
+//part3
+
+// varsCoin.createTransaction(new Transaction('address1', 'address2', 100));
+// varsCoin.createTransaction(new Transaction('address2', 'address1', 50));
+
+// console.log('\n Starting the miner...');
+// varsCoin.minePendingTransactions('varsiers-address');
+
+// console.log('\nBalance of varsiers is', varsCoin.getBalanceOfAddress('varsiers-address'));
+
+// console.log('\n Starting the miner again...');
+// varsCoin.minePendingTransactions('varsiers-address');
+
+// console.log('\nBalance of varsiers is', varsCoin.getBalanceOfAddress('varsiers-address'));
+
+// console.log('\n Starting the miner again...');
+// varsCoin.minePendingTransactions('varsiers-address');
+
+// console.log('\nBalance of varsiers is', varsCoin.getBalanceOfAddress('varsiers-address'));
+
+//part1
+// varsCoin.addBlock(new Block(1, "10/02/2023", { amount: 4}));
+// varsCoin.addBlock(new Block(2, "15/03/2023", { amount: 10}));
+
+// // console.log(JSON.stringify(varsCoin, null, 4));
+// console.log('Is blockchain valid?' + varsCoin.isChainValid());
+
+// varsCoin.chain[1].data = {amount: 100};
+// varsCoin.chain[1].hash = varsCoin.chain[1].calculateHash();
+
+// console.log('Is blockchain valid?' + varsCoin.isChainValid());
+
+//part2
+// console.log("Mining block 1...");
+// varsCoin.addBlock(new Block(1, "10/01/2023", { amount: 4}));
+// console.log("Mining block 2...");
+// varsCoin.addBlock(new Block(2, "15/02/2023", { amount: 10}));
